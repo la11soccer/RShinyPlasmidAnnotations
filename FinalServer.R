cfe1eb240b1546931ceae264248bd2094a45631a
la11soccer <50342885+la11soccer@users.noreply.github.com>
2021-08-20 11:39:04 -0400

    Update FinalServer.R

--- FinalServer.R
+++ FinalServer.R
@@ -1,13 +1,16 @@
+#import statements
 library(shiny)
 library(reticulate)
 library(ggplot2)
 library(rlang)
-
 os <- import("os")
-# Define server logic required to draw a histogram
+
+#use retuculate package to use python script
 reticulate::source_python('~/Documents/HonorsProject/outDir/FunctionTest.py')
 
-withConsoleRedirect <- function(containerId, expr) {
+# block of code below is an example of what getting PROKKA run output to user might look like  
+#######################################################
+'''withConsoleRedirect <- function(containerId, expr) {
     # Change type="output" to type="message" to catch stderr
     # (messages, warnings, and errors) instead of stdout.
     txt <- capture.output(results <- expr, type = "message")
@@ -17,35 +20,39 @@ withConsoleRedirect <- function(containerId, expr) {
         )
     }
     results
-}
-
+}'''
+##########################################################
+#shiny server function
 shinyServer(function(input, output) {
     
     #function to give the filepath of input file
     file_name <-reactive({
         inFile <- input$file
-        #print(inFile$datapath)
-        #print(getwd())
         return(inFile$datapath)
     })
     
-    #print(paste("file_name: ",file_name))
-    
+    #function observes user action for when they press a button
     observeEvent(input$button,{
         
+            #fasta file input by user
            example_file.fasta <- file_name()
              example_file.fasta
-            
+                    #remove the results from previous PROKKA run
                     system(paste("rm ~/Documents/HonorsProject/outDir/ourDir1/PROKKA*"))
+                    #call to PROKKA with new user input file
                     system(paste("prokka --outdir ~/Documents/HonorsProject/outDir/ourDir1 --force --proteins ~/Documents/HonorsProject/outDir/Summer19CompleteDatabase.faa --evalue 0.001 --addgenes ", example_file.fasta))
                     
-                    #get the tbl file so we can pass it into the scripts
+                    #get the tbl file from PROKKA output so we can pass it into the scripts
                     files <- list.files(path = "~/Documents/HonorsProject/outDir/ourDir1", pattern = "tbl")
-                    print(files)
+                    #print(files)
+                    #set directory to direct the output files from the python script run 
                     setwd("~/Documents/HonorsProject/outDir/ourDir1")
-                    fullRun("PROKKA",files)
-                    print("here")
                     
+                    #call python script on on tbl file 
+                    fullRun("PROKKA",files)
+                    #print("here")
+                    
+                    #code for histogram 
                     output$plot <- renderPlot({
                     inFile = "PROKKARCompatibleTable.csv"
                     df <- read.csv(inFile, header = TRUE)
@@ -53,30 +60,25 @@ shinyServer(function(input, output) {
                     #p + coord_cartesian(ylim = c(0,20))
                     p + theme(axis.text.x = element_text(angle = 90) ) + geom_bar() 
                     
-                   
                     })
-                    print("done")
-    
+                    print("done") #print to council
+    #allow for user download of files
     output$downloadData <- downloadHandler(
-       
+       #zip files for download
         filename = function() {
             paste(input$outputFile, ".zip", sep="")
         },
         content = function(file){
-            
-            
             fileCopied <- c()
-            
             setwd("~/Documents/HonorsProject/outDir/ourDir1")
+            #do not include python script output files in downloaded files - advised by Dr. Page. Python files only needed for visualization
             files <- list.files(path = "~/Documents/HonorsProject/outDir/ourDir1", pattern = "[^(csv)]$")
             for (i in 1:length(files)){
                 fileCopied <- c(paste(files[i]), fileCopied)
             }
-            print(files)
-            
+            #print(files)
+            #zip up appropriate files
             zip(zipfile = file, files = fileCopied)
-            
-            
         },
         contentType = "application/zip")
     
